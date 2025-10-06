bcdef5355010b2ab1e9dc7dedc85565e8cf7f21d
AhmedSolis <sahmedrasheed@gmail.com>
2025-10-05 22:05:25 -0600

    Avance CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Escuchar el puerto (solo una vez)
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
